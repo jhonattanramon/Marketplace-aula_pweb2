0822e32f8a52ca665c3ea4a99170ab2aa50e313f
jhonattanRamon <jhonattan.ramon.17@gmail.com>
2023-04-05 16:14:41 -0300

    /

--- my-app/src/components/Jogos_Component.jsx
+++ my-app/src/components/Jogos_Component.jsx
@@ -1,10 +1,24 @@
 
 const Jogos_component = (props) => {
+<<<<<<< HEAD
+=======
 
+ 
+
+
+    if (props.produtos.length === 0 ) {
+      return  
+    }
+
+>>>>>>> parent of 52c69d8 (Merge pull request #4 from jhonattanramon/David/Jefferson)
 
     return(
         <div>
+<<<<<<< HEAD
              <div>pr</div>
+=======
+             
+>>>>>>> parent of 52c69d8 (Merge pull request #4 from jhonattanramon/David/Jefferson)
         </div>
     )
 }
