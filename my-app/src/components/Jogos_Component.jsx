7bce467ae02eda99bdd549131d64316c307fb338
adrielleharaujo <110988623+adrielleharaujo@users.noreply.github.com>
2023-04-05 13:29:55 -0300

    upgrade
    código melhorado.

--- my-app/src/components/Jogos_Component.jsx
+++ my-app/src/components/Jogos_Component.jsx
@@ -1,12 +1,77 @@
+import './jogos_estilos.css'
 
 const Jogos_component = (props) => {
+    if (props.produtos.length === 0) {
+        return
+    }
+
+    return (
+        <div id='geral'>
+
+            <div class='jogo'>
+                <p>{props.produtos.jogos[0].nome}</p>
+                <img src={props.produtos.jogos[0].imagem} alt="" />
+                <p>{props.produtos.jogos[0].preco}</p>
+            </div>
+
+            <div class='jogo'>
+                <p>{props.produtos.jogos[1].nome}</p>
+                <img src={props.produtos.jogos[1].imagem} alt="" />
+                <p>{props.produtos.jogos[1].preco}</p>
+            </div>
+
+            <div class='jogo'>
+                <p>{props.produtos.jogos[2].nome}</p>
+                <img src={props.produtos.jogos[2].imagem} alt="" />
+                <p>{props.produtos.jogos[2].preco}</p>
+            </div>
+
+            <div class='jogo'>
+                <p>{props.produtos.jogos[3].nome}</p>
+                <img src={props.produtos.jogos[3].imagem} alt="" />
+                <p>{props.produtos.jogos[3].preco}</p>
+            </div>
+
+            <div class='jogo'>
+                <p>{props.produtos.jogos[4].nome}</p>
+                <img src={props.produtos.jogos[4].imagem} alt="" />
+                <p>{props.produtos.jogos[4].preco}</p>
+            </div>
+
+            <div class='jogo'>
+                <p>{props.produtos.jogos[5].nome}</p>
+                <img src={props.produtos.jogos[5].imagem} alt="" />
+                <p>{props.produtos.jogos[5].preco}</p>
+            </div>
+
+            <div class='jogo'>
+                <p>{props.produtos.jogos[6].nome}</p>
+                <img src={props.produtos.jogos[6].imagem} alt="" />
+                <p>{props.produtos.jogos[6].preco}</p>
+            </div>
+
+            <div class='jogo'>
+                <p>{props.produtos.jogos[7].nome}</p>
+                <img src={props.produtos.jogos[7].imagem} alt="" />
+                <p>{props.produtos.jogos[7].preco}</p>
+            </div>
+
+            <div class='jogo'>
+                <p>{props.produtos.jogos[8].nome}</p>
+                <img src={props.produtos.jogos[8].imagem} alt="" />
+                <p>{props.produtos.jogos[8].preco}</p>
+            </div>
+
+            <div class='jogo'>
+                <p>{props.produtos.jogos[9].nome}</p>
+                <img src={props.produtos.jogos[9].imagem} alt="" />
+                <p>{props.produtos.jogos[9].preco}</p>
+            </div>
 
 
-    return(
-        <div>
-             <div>pr</div>
+
         </div>
     )
 }
- 
+
 export default Jogos_component
