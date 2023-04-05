455febd3e0c1a077264a6815b28861916198133b
Yasmin Jandriele <yasmin.carvalho@igdema.ufal.br>
2023-04-05 16:06:51 -0300

    Create Bebidas_component

---
+++ my-app/src/components/Bebidas_component.jsx
@@ -0,0 +1,14 @@
+
+
+const Bebidas_component = () => {
+
+  return(
+      <div>
+          <div>
+              favoritos
+          </div>
+      </div>
+  )
+}
+
+export default Bebidas_component
