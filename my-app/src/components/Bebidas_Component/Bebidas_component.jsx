b715c2f894a6110b94eed7468bf255086fe9ff4e
Yasmin Jandriele <yasmin.carvalho@igdema.ufal.br>
2023-04-13 15:53:50 -0300

    oi

--- my-app/src/components/Bebidas_Component/Bebidas_component.jsx
+++ my-app/src/components/Bebidas_Component/Bebidas_component.jsx
@@ -25,7 +25,6 @@ const Bebidas_component = (props) => {
               </div>
             </div>
 
-<<<<<<< Updated upstream
             <div className="fav">
               <button
                 className="buttonFav"
@@ -35,11 +34,11 @@ const Bebidas_component = (props) => {
                     props.produtos.bebida,
                   ]);
                 }}
-              >
-                FAVORITAR
-              </button>{" "}
+              >FAVORITAR</button>
               <br />
             </div>
+
+
             <div className="den">
               <button className="buttonDen">DENUNCIAR</button>
             </div>
@@ -49,54 +48,5 @@ const Bebidas_component = (props) => {
     </div>
   );
 };
-=======
-    if (props.produtos.length === 0) {
-        return null;
-    }
-
-  return(
-    
-<div className="containerBebidas">
-
-{props.produtos.bebidas.map((bebida) =>{
-
-    
-    
-    return  <div className="cardBebidas" key={bebida.id}>
-        <img className="cardBebidas1" src={bebida.imagem} alt="logo das marcas"/>
-        <div className="cardH3">
-        <h3>{bebida.nome} <br /></h3> 
-        <div className= 'cardInfo'>
-          <h3>
-        {bebida.descricao}</h3> 
-        <h3>R${bebida.preco}</h3> 
-        
-        </div>
-        </div>
-   
-        <div className="fav"><button        className="buttonFav"                            onClick={() => {
-          props.onAddFavoritos([...props.PropFavoritos, props.produtos.bebidas[6]]);}}>FAVORITAR</button> <br /></div>
-        <div className="den"><button className="buttonDen">DENUNCIAR</button></div>
-
-          
-      
-       
-      
-       
-        
-       
-       
-  </div>
-})}
-</div>
-    
-        
-  )
-}
-
-export default Bebidas_component
-
-
->>>>>>> Stashed changes
 
 export default Bebidas_component;
