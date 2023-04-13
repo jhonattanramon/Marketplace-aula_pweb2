73f3d444c10c7a2443945ac8b97c1b52205036a2
Yasmin Jandriele <yasmin.carvalho@igdema.ufal.br>
2023-04-13 15:43:28 -0300

    oi

--- my-app/src/components/Bebidas_Component/Bebidas_component.jsx
+++ my-app/src/components/Bebidas_Component/Bebidas_component.jsx
@@ -25,6 +25,7 @@ const Bebidas_component = (props) => {
               </div>
             </div>
 
+<<<<<<< Updated upstream
             <div className="fav">
               <button
                 className="buttonFav"
@@ -48,5 +49,54 @@ const Bebidas_component = (props) => {
     </div>
   );
 };
+=======
+    if (props.produtos.length === 0) {
+        return null;
+    }
+
+  return(
+    
+<div className="containerBebidas">
+
+{props.produtos.bebidas.map((bebida) =>{
+
+    
+    
+    return  <div className="cardBebidas" key={bebida.id}>
+        <img className="cardBebidas1" src={bebida.imagem} alt="logo das marcas"/>
+        <div className="cardH3">
+        <h3>{bebida.nome} <br /></h3> 
+        <div className= 'cardInfo'>
+          <h3>
+        {bebida.descricao}</h3> 
+        <h3>R${bebida.preco}</h3> 
+        
+        </div>
+        </div>
+   
+        <div className="fav"><button        className="buttonFav"                            onClick={() => {
+          props.onAddFavoritos([...props.PropFavoritos, props.produtos.bebidas[6]]);}}>FAVORITAR</button> <br /></div>
+        <div className="den"><button className="buttonDen">DENUNCIAR</button></div>
+
+          
+      
+       
+      
+       
+        
+       
+       
+  </div>
+})}
+</div>
+    
+        
+  )
+}
+
+export default Bebidas_component
+
+
+>>>>>>> Stashed changes
 
 export default Bebidas_component;
