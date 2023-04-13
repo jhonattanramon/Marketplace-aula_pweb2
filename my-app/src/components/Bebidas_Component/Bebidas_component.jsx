987f58aef5e093dbc533485b5f94f5c5c4898132
jhonattanRamon <jhonattan.ramon.17@gmail.com>
2023-04-13 15:39:02 -0300

    .

--- my-app/src/components/Bebidas_Component/Bebidas_component.jsx
+++ my-app/src/components/Bebidas_Component/Bebidas_component.jsx
@@ -1,53 +1,52 @@
 import "./bebidas.css";
 
 const Bebidas_component = (props) => {
+  if (props.produtos.length === 0) {
+    return null;
+  }
 
+  return (
+    <div className="containerBebidas">
+      {props.produtos.bebidas.map((bebida) => {
+        return (
+          <div className="cardBebidas" key={bebida.id}>
+            <img
+              className="cardBebidas1"
+              src={bebida.imagem}
+              alt="logo das marcas"
+            />
+            <div className="cardH3">
+              <h3>
+                {bebida.nome} <br />
+              </h3>
+              <div className="cardInfo">
+                <h3>{bebida.descricao}</h3>
+                <h3>R${bebida.preco}</h3>
+              </div>
+            </div>
 
-    if (props.produtos.length === 0) {
-        return null;
-    }
+            <div className="fav">
+              <button
+                className="buttonFav"
+                onClick={() => {
+                  props.onAddFavoritos([
+                    ...props.PropFavoritos,
+                    props.produtos.bebida,
+                  ]);
+                }}
+              >
+                FAVORITAR
+              </button>{" "}
+              <br />
+            </div>
+            <div className="den">
+              <button className="buttonDen">DENUNCIAR</button>
+            </div>
+          </div>
+        );
+      })}
+    </div>
+  );
+};
 
-  return(
-    
-<div className="containerBebidas">
-
-{props.produtos.bebidas.map((bebida) =>{
-
-    
-    
-    return  <div className="cardBebidas" key={bebida.id}>
-        <img className="cardBebidas1" src={bebida.imagem} alt="logo das marcas"/>
-        <div className="cardH3">
-        <h3>{bebida.nome} <br /></h3> 
-        <div className= 'cardInfo'>
-          <h3>
-        {bebida.descricao}</h3> 
-        <h3>R${bebida.preco}</h3> 
-        
-        </div>
-        </div>
-   
-        <div className="fav"><button        className="buttonFav"                            onClick={() => {
-          onAddFavoritos([...props.PropFavoritos, props.produtos.bebida]);}}>FAVORITAR</button> <br /></div>
-        <div className="den"><button className="buttonDen">DENUNCIAR</button></div>
-
-          
-      
-       
-      
-       
-        
-       
-       
-  </div>
-})}
-</div>
-    
-        
-  )
-}
-
-export default Bebidas_component
-
-
-
+export default Bebidas_component;
