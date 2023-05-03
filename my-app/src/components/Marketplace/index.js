dc672ab760e25ea79625d58658ceec86d64b2e02
jhonattanRamon <jhonattan.ramon.17@gmail.com>
2023-05-03 15:13:23 -0300

    .

--- my-app/src/components/Marketplace/index.js
+++ my-app/src/components/Marketplace/index.js
@@ -1,9 +1,7 @@
 import "../../App.css";
 import React from "react";
 import { useEffect, useState } from "react";
-
 //component
-
 import Filmes_component from "../Filmes_Component/Filmes_component";
 import Jogos_component from "../Jogos_Component/Jogos_Component";
 import Roupas_component from "../Roupas_componet/Roupas_component";
@@ -12,18 +10,12 @@ import Bebidas_component from "../Bebidas_Component/Bebidas_component";
 import Favoritos_component from "../Favoritos_Component/Favoritos_component";
 import Denuncia_component from "../Denuncia_component.s/Denuncia_component";
 
-
-
 const Marketplace = () => {
-
-
-
-
   const [produtos, setProdutos] = useState([]);
   const [favoritos, setFavoritos] = useState([]);
   const [denunciados, setDenunciados] = useState([]);
-
   const [totalFavoritos, setTotalFavoritos] = useState(0);
+ 
 
   const addDenunciados = (produto) => {
     const checkProduto = denunciados.find((f) => f.id === produto.id);
@@ -42,33 +34,35 @@ const Marketplace = () => {
     }
   };
 
-  useEffect(() => {
+  useEffect( () => {
     const load = async () => {
       const resultJson = await fetch("https://fakestoreapi.com/products");
 
-      const fakeStoreaApi = await resultJson.json();
+      const resultProduct = await resultJson.json();
 
       //formatação da API FAKE
-      const apiFomatada = [];
-
-      fakeStoreaApi.map(({ id, title, price, image }) => {
-        apiFomatada.push({
-          id: id,
-          nome: title,
-          preco: price,
-          imagem: image,
-        });
-      });
+      let apiFomatada = resultProduct.map(({ id, title, price, image }) => ({
+        id: id,
+        nome: title,
+        preco: price,
+        imagem: image,
+      }));
 
       setProdutos(apiFomatada);
     };
+
     load();
-  }, []);
+  },[] );
+
+
+
+ 
 
   return (
     <div className="App">
       <div className="box2">
         <h1>{totalFavoritos}</h1>
+       
         <Favoritos_component
           favoritos={favoritos}
           onAddFavoritos={addFavoritos}
@@ -98,8 +92,6 @@ const Marketplace = () => {
       </div>
 
       <div className="box2">
-
-         
         <Roupas_component
           produtos={produtos}
           PropFavoritos={favoritos}
@@ -127,8 +119,6 @@ const Marketplace = () => {
       </div>
     </div>
   );
-}
+};
 
-
-
-export default Marketplace
+export default Marketplace;
