2b040127f32299f750d428af521faeb3162cddb4
esterarab <118554329+esterarab@users.noreply.github.com>
2023-05-04 16:33:58 -0300

    commit ester

--- my-app/src/components/Filmes_Component/Filmes_component.jsx
+++ my-app/src/components/Filmes_Component/Filmes_component.jsx
@@ -1,13 +1,86 @@
+import { useEffect, useState } from "react";
 import "./filmes.css";
 const Filmes_component = (props) => {
+
+  const [laptops, setLaptops] = useState([]);
+
+  useEffect(() => {
+
+    const load = async () => {
+      const resultJson = await fetch("https://dummyjson.com/products/category/laptops");
+
+      const fakeStoreaApi = await resultJson.json();
+
+      const apiFomatada = [];
+
+      fakeStoreaApi.products.map(({ id, title, price, thumbnail, rating, stock }) => {
+        apiFomatada.push({
+          id: id,
+          nome: title,
+          preco: price, 
+          imagem: thumbnail,
+          avaliacao: rating,
+          estoque: stock,
+        });
+      });
+
+      setLaptops(apiFomatada);
+    };
+
+    load();
+  }, []);
+
   if (props.produtos.length === 0) {
     return;
   }
 
+
   const setProps = props.setProps;
 
   return (
+
     <>
+
+<div className="title">
+        {" "}
+        <h1>LAPTOPS</h1>
+      </div>
+      <div className="card">
+        {laptops.map((laptop) => {
+          return (
+            <section className="sectionContainer" key={laptop.id}>
+              <div>
+                <img className="img" src={laptop.imagem} />
+              </div>
+
+              <div className="divDescription">
+                <div>{laptop.nome}</div>
+                <div>{laptop.preco}</div>
+                <div>{laptop.avaliacao}</div>
+                <div>{laptop.estoque}</div>
+
+                <div>
+                  <button
+                    onClick={() => {
+                      props.onAddFavoritos(props.produtos[Number(laptop.id) - 1]);
+                    }}
+                  >
+                    Adicionar Favoritos
+                  </button>
+                  <button
+                    onClick={() => {
+                      props.onAddDenuncia(props.produtos[Number(laptop.id) - 1]);
+                    }}
+                  >
+                    Denunciar
+                  </button>
+                </div>
+              </div>
+            </section>
+          );
+        })}
+      </div>
+
       <div className="title">
         {" "}
         <h1>FILMES</h1>
