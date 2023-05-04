d224cae8f82eba0aaea201c14835955e10030042
Pedrix55 <pedrogabriel12gamer@outlook.com>
2023-05-04 16:28:43 -0300

    UserState
    ARRUMAR O IF DO SELECT

--- my-app/src/components/Roupas_componet/Roupas_component.jsx
+++ my-app/src/components/Roupas_componet/Roupas_component.jsx
@@ -1,28 +1,92 @@
 import "./roupas.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 const Roupas_component = ({
-  produtos,
+  
   onAddFavoritos,
   onAddDenuncia,
   PropFavoritos,
 }) => {
 
+  // if (produtos.length === 0) {
+  //   return;
+  // }
+
 
   const [contagem, setContagem] = useState(0)
 
+  const [produtos, setProdutos] = useState([])
 
-  if (produtos.length === 0) {
-    return;
+  const [ordem, setOrdem] = useState('preco')
+
+  useEffect (() => {
+
+   async function loading () {
+    const responsive = await fetch('https://dummyjson.com/products/category/mens-shirts')
+
+    const result = await responsive.json();   
+    console.log(result.products)
+
+    const selectionProduct = result.products.map(({title, thumbnail, price, stock, rating}) => (
+      {
+        nome: title,
+        imagem: thumbnail,
+        preco: price,
+        estoque: stock,
+        avaliacao: rating
+      }
+    ))
+
+    setProdutos(selectionProduct)
+
   }
+    loading()
+  },[]);
 
+
+  useEffect (() =>{
+
+     function loading (params) {
+
+        console.log(produtos);
+       const format = produtos.sort(function(a,b){
+      
+        console.log(a.preco);
+        console.log(b.preco);        
+        if(true){
+          return -1
+        }else{
+         return 0
+        }
+          
+        
+    })
+
+    console.log(format);
+     
+     
+      
+     } 
+      loading()
+  },[ordem])
+  
   return (
 
     <> 
       <div className="title"> <h1>ROUPAS <label> {contagem}</label></h1> 
-        
+      <select name="select" onChange={(event) => {
+        setOrdem(event.target.value)
+
+      }}>
+  <option value="valor1">Maior Preço</option>
+  <option value="valor2" >Menor Preço</option>
+  <option value="valor3">Mair estoque</option>
+  <option value="valor4">Mais Avaliados</option>
+</select>
       </div>
 
     <div className="card">
+
+   
 
 
       {produtos.map(({ imagem, nome, descricao, preco, id }) => {
