520fd1f08feebbfda3e4bafedcd169bbfcbb3429
End-Star01 <anavanessa162@gmail.com>
2023-05-10 16:13:06 -0300

    ana

--- my-app/src/components/Roupas_componet/Roupas_component.jsx
+++ my-app/src/components/Roupas_componet/Roupas_component.jsx
@@ -34,10 +34,7 @@ const Roupas_component = ({ produtos, setProps, PropFavoritos }) => {
               <div>
                 <button
                   onClick={() => {
-                    setProps([
-                      ...PropFavoritos,
-                      produtos.roupas[Number(id) - 1],
-                    ]);
+                    props.onAddDenuncia(props.produtos,roupas[0]);
                   }}
                 >
                   adicionar favoritos
