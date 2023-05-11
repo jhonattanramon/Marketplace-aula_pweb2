f23b076b229a978479464d897f76fc3380077798
esterarab <118554329+esterarab@users.noreply.github.com>
2023-05-11 15:31:53 -0300

    filmes
    atualização

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,15 +1,18 @@
  import "./App.css";
 import React from "react";
+import { Routes, Route } from "react-router-dom";
 
 //component
 import Marketplace from "./components/Marketplace";
+import LaptopPageComponent from "./components/Filmes_Component/LaptopPageComponent";
 
 function App() {
   
   return(
-    <>
-    <Marketplace /> 
-    </>
+  <Routes>
+    <Route path="/" element={<Marketplace/>}></Route>
+    <Route path="filmes" element={<LaptopPageComponent/>}></Route>
+  </Routes>
   )
 }
 
