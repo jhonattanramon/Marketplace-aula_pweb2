a77ef9ac2d316c1e7aa05c96a298ba128a6291a8
ValterSoarees <valter.soareess@gmail.com>
2023-05-17 16:04:56 -0300

    atualizando rota

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,4 +1,4 @@
- import "./App.css";
+import "./App.css";
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import FRoupasHome from "./components/FRoupasHome";
@@ -13,9 +13,9 @@ function App() {
     <Routes>
         <Route path="/" element={<Marketplace/>}></Route>
         <Route path="FRoupasHome" element={<FRoupasHome/>}></Route>
-        <Route path="Relogios" element={<RelogioPageComponent/>}></Route>
+        <Route path="RelogioPageComponent" element={<RelogioPageComponent/>}></Route>
       </Routes>
   )
-}
+} 
 
 export default App;
