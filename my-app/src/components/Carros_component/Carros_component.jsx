4b27300636d8e218d1bb46b5b5a7e8ab392f6562
VitoriaGoncalvez <118554164+IderlanOliveira@users.noreply.github.com>
2023-05-25 13:38:44 -0300

    Update Carros_component.jsx

--- my-app/src/components/Carros_component/Carros_component.jsx
+++ my-app/src/components/Carros_component/Carros_component.jsx
@@ -1,29 +1,31 @@
 const Carros_component = () => {
 
+  useEffect( () => {
+
+    const load = async () => {
+      const resultJson = await fetch("https://fakestoreapi.com/products");
+    
+      const resultProduct = await resultJson.json();
+    
+      console.log();
+      //formatação da API FAKE
+      let apiFormatada = resultProduct.map(({ id, title, price, image }) => ({
+        id: id,
+        nome: title,
+        preco: price,
+        imagem: image,
+      }));
+    
+      setProdutos(apiFormatada);
+    };
+    
+    load();
+    },[] );
+
   return(
       <div>
           <div>
-          useEffect( () => {
-
-const load = async () => {
-  const resultJson = await fetch("https://fakestoreapi.com/products");
-
-  const resultProduct = await resultJson.json();
-
-  console.log();
-  //formatação da API FAKE
-  let apiFormatada = resultProduct.map(({ id, title, price, image }) => ({
-    id: id,
-    nome: title,
-    preco: price,
-    imagem: image,
-  }));
-
-  setProdutos(apiFormatada);
-};
-
-load();
-},[] );
+        
           </div>
       </div>
   )
